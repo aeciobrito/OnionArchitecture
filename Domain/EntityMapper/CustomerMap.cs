e30ccdc925927d4ae5a405a1707262b2dc168a95
Aecio Brito Ideapad <aeciobrito@gmail.com>
2022-02-17 15:52:38 -0300

    Domain entity and entity map

---
+++ Domain/EntityMapper/CustomerMap.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.EntityMapper
+{
+    internal class CustomerMap : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(p => p.Id)
+                .HasName("pk_customerid");
+
+            builder.Property(p => p.Id).ValueGeneratedOnAdd()
+                .HasColumnName("Id")
+                .HasColumnType("INT");
+
+            builder.Property(p => p.PurchasesProduct)
+                .HasColumnName("purchased_product")
+                .HasColumnType("NVARCHAR(100)")
+                .IsRequired();
+
+            builder.Property(x => x.PaymentType)
+               .HasColumnName("payment_type")
+                  .HasColumnType("NVARCHAR(50)")
+                  .IsRequired();
+
+            builder.Property(x => x.CreatedDate)
+              .HasColumnName("created_date")
+              .HasColumnType("datetime");
+
+            builder.Property(x => x.ModifiedDate)
+              .HasColumnName("modified_date")
+              .HasColumnType("datetime");
+
+            builder.Property(x => x.IsActive)
+              .HasColumnName("is_active")
+              .HasColumnType("bit");
+        }
+    }
+}
---
+++ Domain/Models/Customer.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    internal class Customer : Entity
+    {
+        public Customer(string customerName, string purchasesProduct, string paymentType)
+        {
+            CustomerName = customerName;
+            PurchasesProduct = purchasesProduct;
+            PaymentType = paymentType;
+            CreatedDate = DateTime.Now;
+            IsActive = true;
+        }
+
+        #region Entity
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public bool IsActive { get; set; }
+       
+        public bool Validate()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        public string CustomerName { get; private set; } = string.Empty;
+        public string PurchasesProduct { get; private set; } = string.Empty;
+        public string PaymentType { get; private set; } = string.Empty;
+
+       
+    }
+}
